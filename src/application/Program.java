a1a535d53da4768a09ee24d480d93355fe378ff0
João Garske <91497473+Joaogarske@users.noreply.github.com>
2024-01-02 16:50:04 -0300

    class seller adicionada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,21 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dp = new Department(1,"Books");
-		System.out.println(dp);
-
+		Department obj = new Department(1,"Books");
+		
+		
+		
+		Seller seller = new  Seller(21, "joao", "joao@gmail.com", new Date(), 3000.00, obj);
+		
+        System.out.println(seller);
 	}
 
 }
