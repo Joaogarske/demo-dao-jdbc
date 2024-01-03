4185d9bbb32ce93c7b3499c894381c44c3c4609c
João Garske <91497473+Joaogarske@users.noreply.github.com>
2024-01-03 10:19:32 -0300

    adicionado method findById class SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,18 +2,20 @@ package application;
 
 import java.util.Date;
 
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1,"Books");
+	
 		
-		
-		
-		Seller seller = new  Seller(21, "joao", "joao@gmail.com", new Date(), 3000.00, obj);
+		SellerDao sellerDao = DaoFactory.createSelleDao();
+
+		Seller seller = sellerDao.findById(1);
 		
         System.out.println(seller);
 	}
